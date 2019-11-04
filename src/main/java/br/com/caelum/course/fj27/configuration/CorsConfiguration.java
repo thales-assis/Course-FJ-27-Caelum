fcf5fffea13723ecb117abf3a15c8ebafc39309a
Thales Assis <thaleslima-19@hotmail.com>
2019-11-04 12:52:24 -0300

    CORS Configuration

---
+++ src/main/java/br/com/caelum/course/fj27/configuration/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package br.com.caelum.course.fj27.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
+
+}
