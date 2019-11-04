4b79a00a652d9d46306d9fcadd74be20193fbaf2
Thales Assis <thaleslima-19@hotmail.com>
2019-11-04 11:58:53 -0300

    First commit

---
+++ src/test/java/br/com/caelum/course/fj27/CaelumCourseFj27ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.course.fj27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaelumCourseFj27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
